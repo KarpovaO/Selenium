241d52b56e5cf988c6d9ed4c6dd13e3cd85ca5b6
Olga <88337282+KarpovaO@users.noreply.github.com>
2023-03-20 22:39:34 +0300

    Update CallbackTest.java

--- api-ci/rest/src/test/java/CallbackTest.java
+++ api-ci/rest/src/test/java/CallbackTest.java
@@ -22,10 +22,9 @@ class CallbackTest {
         options.addArguments("--disable-gpu"); // applicable to windows os only
         options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
         options.addArguments("--no-sandbox"); // Bypass OS security model
-        options.setExperimentalOption("useAutomationExtension", false);
-        options.addArguments("--remote-debugging-port=9222");
-        options.addArguments("--user-data-dir=~/.config/google-chrome");
-        options.addArguments("--profile-directory=Default");
+        //options.setExperimentalOption("useAutomationExtension", false);
+        //options.addArguments("--remote-debugging-port=9222");
+        options.setBinary(new File("/usr/bin/chromium-browser"));
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
     }

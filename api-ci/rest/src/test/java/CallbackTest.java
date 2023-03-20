428ebfd3cd659e65e484524eb2397546dac74de2
Olga <88337282+KarpovaO@users.noreply.github.com>
2023-03-20 23:49:11 +0300

    Update CallbackTest.java

--- api-ci/rest/src/test/java/CallbackTest.java
+++ api-ci/rest/src/test/java/CallbackTest.java
@@ -25,15 +25,12 @@ class CallbackTest {
         options.addArguments("--no-sandbox"); // Bypass OS security model
         options.setExperimentalOption("useAutomationExtension", false);
         options.addArguments("--remote-debugging-port=9222");
-        //options.setBinary(new File("/usr/bin/google-chrome-stable"));
-        //options.setBinary(new File("/usr/bin/google-chrome"));
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
     }
 
     @BeforeEach
     void setup() {
-        //driver = new ChromeDriver();
     }
 
     @AfterEach

16dd6606e2a80a1fb992014f544f26f108a1218b
Olga <88337282+KarpovaO@users.noreply.github.com>
2023-03-20 22:42:49 +0300

    Update CallbackTest.java

--- api-ci/rest/src/test/java/CallbackTest.java
+++ api-ci/rest/src/test/java/CallbackTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeOptions;
+import java.io.File;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 

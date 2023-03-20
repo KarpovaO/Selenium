9c25fb78f52052a0d2c55c99f8815b0e2fff7006
KarpovaO <grose@yandex.ru>
2023-03-20 13:41:48 +0300

    refresh

--- api-ci/rest/src/test/java/CallbackTest.java
+++ api-ci/rest/src/test/java/CallbackTest.java
@@ -22,6 +22,7 @@ class CallbackTest {
         options.addArguments("--disable-gpu"); // applicable to windows os only
         options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
         options.addArguments("--no-sandbox"); // Bypass OS security model
+        options.setExperimentalOption("useAutomationExtension", false);
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
     }
